db9d829bfa8ebce3af5e1ca6790755ab3e696794
PrashansaKulshreshtha <pkul8468@gmail.com>
2025-08-24 11:42:24 +0530

    Remove backend folder, moved to separate repo

--- careconnect/src/main/java/com/careconnect/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.careconnect.repository;
-
-import com.careconnect.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- careconnect/src/main/java/com/careconnect/repository/EMRRepository.java
+++
@@ -1,7 +0,0 @@
-package com.careconnect.repository;
-
-import com.careconnect.model.EMR;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EMRRepository extends JpaRepository<EMR, Long> {
-}
